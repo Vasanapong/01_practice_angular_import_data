78b9cdc42b2b6777ef6e1fb76759058f4a670fff
Vasanapong <vasanapong.c@gmail.com>
2021-12-08 00:52:42 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+declare const quiz_data:any
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  handleClick = () =>{
+    console.log(quiz_data)
+  }
+}
+
+// Create Array In Eternal JavaScript File ( dont need to export )
+// Place path in angular.json > script
+// delcare const in component
+// test call in function
---
+++ src/app/quiz_data/quiz_data.js
@@ -0,0 +1,42 @@
+const quiz_data = [
+    {
+        question:'What does HTML stand for ?',
+        answer:'A',
+        A:'Hyper Text Markup Language',
+        B:'Hyper Text Multiple Language',
+        C:'Hyper Tool Markup Language',
+        D:'Home Tool Management Language',
+    },
+    {
+        question:'Which one is JavaScript Framework ?',
+        answer:'C',
+        A:'BootStrap',
+        B:'Flutter',
+        C:'Angular',
+        D:'Tailwind',
+    },
+    {
+        question:'Which one is not CSS Framework ?',
+        answer:'D',
+        A:'Tailwind',
+        B:'BootStrap',
+        C:'Flutter',
+        D:'Bulma',
+    },
+    {
+        question:'Which one is not Database',
+        answer:'B',
+        A:'MongoDB',
+        B:'OtterSQL',
+        C:'MySQL',
+        D:'Oracle',
+    },
+    {
+        question:'Which one is not Programming Language ?',
+        answer:'A',
+        A:'Python',
+        B:'PySpark',
+        C:'JavaScript',
+        D:'Go',
+    }
+]
